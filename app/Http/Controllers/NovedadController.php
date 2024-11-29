1b1de9a8e91aa43634750b619219c2ee7d0b9308
Kelc098 <119705537+Kelc098@users.noreply.github.com>
2024-11-29 17:57:03 -0500

    se agrego traslados

--- app/Http/Controllers/NovedadController.php
+++ app/Http/Controllers/NovedadController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Models\Novedad;
 use App\Models\Producto;
+use App\Models\Vehiculo;
 use App\Models\AsignacionEquipo;
 use App\Models\User;
 
@@ -14,12 +15,69 @@ class NovedadController extends Controller
 {
     public function index()
     {
-        return view('novedades');
+        //PRODUCTOS SELECCIONADOS
+        $productos =  Producto::get(['codigo_interno', 'codigo_equipo_referencia']);
+        //VEHICULOS SELECCIONADOS
+        $vehiculos = Vehiculo::get('placa', 'descripcion_vehiculo');
+        //CODIGOS INTERNOS SELECCIONADOS
+
+        $referencias = $productos->pluck('codigo_equipo_referencia')->toArray();
+
+        $codigosInternos = $productos->pluck('codigo_interno')->toArray();
+
+        return view('novedades', compact('codigosInternos', 'referencias', 'vehiculos'));
     }
     public function store(Request $request)
     {
-
+        //Toma el dato de producto ingresado en el formulario
         $producto = $request->input('producto');
+        //Toma el dato de tipo de novedad ingresado en el formulario
+        $vale = $request->input('tipo_novedad');
+        //Busca el producto de codigo interno
+        $prod = Producto::where('codigo_interno', $producto)
+            ->first();
+        //verifica que el producto exista.
+        if ($prod == null) {
+            return response()->json(['error' => 'No se encontro ningun producto con ese codigo']);
+        };
+
+
+        //inicia el caso de traslado
+        if ($vale == 'Traslado') {
+            $request->validate([
+                'producto' => 'required|string',
+                'fecha' => 'required|date',
+                'tipo_novedad' => 'required|string',
+                'descripcion' => 'required|string',
+                'ubicacion_new' => 'required|string'
+            ]);
+
+            $ubicacion = $request->input('ubicacion_new');
+            $prod = Producto::where('codigo_interno', $producto)->pluck('ubicacion')->first();;
+            $p = Producto::where('codigo_interno', $producto)->first();
+            //$ubi = $prod->ubicacion;
+            $estate = 'Trasladado desde ' . $prod . ' hacia ' . $ubicacion;
+
+            if ($prod === $ubicacion) {
+                return response()->json(['error' => 'No se pueden mover a la misma ciudad.']);
+            }
+
+            Novedad::create([
+                'producto_id' => $p->id,
+                'usuario_id' => null,
+                'descripcion' => $request->descripcion,
+                'fecha_novedad' => $request->fecha,
+                'tipo_novedad' => $request->tipo_novedad,
+                'estado' => $estate,
+            ]);
+
+            $productoAct = Producto::where('id', $p->id)
+                ->update([
+                    'ubicacion' => $ubicacion,
+                ]);
+
+            return response()->json(['success' => 'Novedad registrada correctamente. prueba']);
+        };
 
         $request->validate([
             'producto' => 'required|string',
@@ -30,72 +88,65 @@ class NovedadController extends Controller
 
         $estado = $request->tipo_novedad;
 
-        $prod = Producto::where('codigo_interno', $producto)
-            ->first();
-        if ($prod==null){
-            return response()->json(['error'=>'No se encontro ningun producto con ese codigo']);
-        };
-
         $asig = AsignacionEquipo::where('producto_id', $prod->id)
-        ->where('estado', 'Asignado')
-        ->first();
+            ->where('estado', 'Asignado')
+            ->first();
+        //En caso de que exista una asignacion tome los datos.
         if ($asig !== null) {
             $test = $asig->producto_id;
         } else {
-            // Aquí puedes manejar el caso cuando $asig es null
+            // ingresa el dato null para evitar errores
             $test = null; // O alguna otra lógica
         }
-        //$test = $asig->producto_id;
 
-        if ($test !== null){
-          $user = User::where('id', $asig)
-           ->value('id');
+        //inicia la logica en caso de que exista una asignacion al producto
+        if ($test !== null) {
+            $user = User::where('id', $asig)
+                ->value('id');
 
-           Novedad::create([
-            'producto_id' => $prod->id,
-            'usuario_id' => $user,
-            'descripcion' => $request->descripcion,
-            'fecha_novedad' => $request->fecha,
-            'tipo_novedad' => $request->tipo_novedad,
-            'estado' =>$estado,
-           ]);
-           if ($estado=='Mantenimiento'){
-            $productoAct = Producto::where('id', $prod->id)
-                ->update([
-                    'estado' => 'Mantenimiento',
-                ]);
-           }
-           else if($estado=='Activo'){
-            $productoAct = Producto::where('id', $prod->id)
-            ->update([
-                'estado' => 'Asignado',
-            ]);
-           };
-
-           return response()->json(['success' => 'Mensaje enviado correctamente']);
-        }
-        else {
             Novedad::create([
-            'producto_id' => $prod->id,
-            'usuario_id' => null,
-            'descripcion' => $request->descripcion,
-            'fecha_novedad' => $request->fecha,
-            'tipo_novedad' => $request->tipo_novedad,
-            'estado' => $request->tipo_novedad,
-           ]);
-           if ($estado=='Mantenimiento'){
-            $productoAct = Producto::where('id', $prod->id)
-                ->update([
-                    'estado' => 'Mantenimiento',
-                ]);
-           }
-           else if($estado=='Activo'){
-            $productoAct = Producto::where('id', $prod->id)
-            ->update([
-                'estado' => 'Disponible',
+                'producto_id' => $prod->id,
+                'usuario_id' => $user,
+                'descripcion' => $request->descripcion,
+                'fecha_novedad' => $request->fecha,
+                'tipo_novedad' => $request->tipo_novedad,
+                'estado' => $estado,
             ]);
-           };
-           return response()->json(['exito' => 'Esta producto no cuenta con un usuario asignado']);
+
+            if ($estado == 'Mantenimiento') {
+                $productoAct = Producto::where('id', $prod->id)
+                    ->update([
+                        'estado' => 'Mantenimiento',
+                    ]);
+            } else if ($estado == 'Activo') {
+                $productoAct = Producto::where('id', $prod->id)
+                    ->update([
+                        'estado' => 'Asignado',
+                    ]);
+            };
+
+            return response()->json(['success' => 'Novedad registrada correctamente']);
+        } else {
+            Novedad::create([
+                'producto_id' => $prod->id,
+                'usuario_id' => null,
+                'descripcion' => $request->descripcion,
+                'fecha_novedad' => $request->fecha,
+                'tipo_novedad' => $request->tipo_novedad,
+                'estado' => $request->tipo_novedad,
+            ]);
+            if ($estado == 'Mantenimiento') {
+                $productoAct = Producto::where('id', $prod->id)
+                    ->update([
+                        'estado' => 'Mantenimiento',
+                    ]);
+            } else if ($estado == 'Activo') {
+                $productoAct = Producto::where('id', $prod->id)
+                    ->update([
+                        'estado' => 'Disponible',
+                    ]);
+            };
+            return response()->json(['exito' => 'Esta producto no cuenta con un usuario asignado']);
         }
-        }
+    }
 }
--- resources/views/novedades.blade.php
+++ resources/views/novedades.blade.php
@@ -8,7 +8,7 @@
             @csrf
             <div>
                 <div class="my-6 w-full text-center text-lg">
-                    <h1 ><strong>Registro de novedades </strong></h1>
+                    <h1><strong>Registro de novedades </strong></h1>
                 </div>
                 <div class="grid md:grid-cols-2 md:gap-6">
                     <div class="relative z-0 w-full mb-5 group">
@@ -16,7 +16,7 @@
                             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Codigo</label>
                         <input type="text" id="producto" name="producto"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
-                                required>
+                            required>
                     </div>
                     <div class="relative z-0 w-full mb-5 group">
                         <label for="fecha"
@@ -43,18 +43,56 @@
                         <label for="descripcion"
                             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Descripcion</label>
                         <textarea id="descripcion" name="descripcion"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
-                            required></textarea>
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" required></textarea>
                     </div>
+                </div>
+                <div id="additionalFields" class="hidden space-y-4">
+                    <div class="relative z-0 w-full mb-5 group">
+                        <label for="ubicacion_new"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Destino</label>
+                        <select type="text" id="ubicacion_new" name="ubicacion_new"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
+                            required>
+                            <option value="null">Seleccionar</option>
+                            <option value="Bogota">Bogota</option>
+                            <option value="Barrancabermeja">Barrancabermeja</option>
+                        </select>
+                    </div>
+
                 </div>
                 <div class="grid md:grid-cols-2 md:gap-6">
                     <div>
-                    <button type="submit"
-                        class="mt-4 w-full bg-red-700 text-white font-semibold rounded-lg p-2 hover:bg-red-900 focus:outline-none focus:ring-2 focus:ring-blue-400">Enviar</button>
+                        <button type="submit"
+                            class="mt-4 w-full bg-red-700 text-white font-semibold rounded-lg p-2 hover:bg-red-900 focus:outline-none focus:ring-2 focus:ring-blue-400">Enviar</button>
                     </div>
                 </div>
             </div>
         </form>
     </div>
+    <script>
+        $(function() {
+            //obtenemos los valores de una columna especifica en la cual solo es profesional, ya que el de los productos, lo realizamos en el controlador.
+            var codigoInternos = @json($codigosInternos);
 
-    @endsection
+            var vehiculo = @json($vehiculos->pluck('placa'));
+
+            var referencias = @json($referencias);
+
+            var todoJunto = codigoInternos.concat(referencias, vehiculo);
+
+            $("#producto").autocomplete({
+                source: todoJunto
+            });
+        });
+
+        document.getElementById('tipo_novedad').addEventListener('change', function() {
+            const additionalFields = document.getElementById('additionalFields');
+            if (this.value === 'Traslado') {
+                additionalFields.classList.remove('hidden');
+            } else {
+                additionalFields.classList.add('hidden');
+            }
+        });
+    </script>
+
+@endsection
